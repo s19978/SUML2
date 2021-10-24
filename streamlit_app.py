d1c79feb54a8a12fca5eb211518f88c34c03ca60
Jędrzej Żytka <s19978@pjwstk.edu.pl>
2021-10-24 12:45:47 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.title('Zajęcia 1. Streamlit')
 
 st.header('Translator EN - DE')
 
-st.text('Aplikacja pozwala na translację języka angielskiego na niemiecki')
-st.text('używając wytrenowanego modelu uczenia maszynowego T5.')
+st.text('Aplikacja pozwala na translację języka angielskiego na niemiecki.')
+st.text('Używa do tego wytrenowanego modelu uczenia maszynowego T5.')
 st.text('')
 st.text('')
 
